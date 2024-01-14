7b5465945bc9fc8be8848437f2474151307be037
aditi-keerti <aditibhadoriya1333@gmail.com>
2024-01-15 03:02:54 +0530

    repo1

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use(cors())
 app.use("/users",userRoute);
 app.use("/blogs",blogRoute)
-
+app.get("/",(req,res)=>{
+    res.send("home page")
+})
 
 
 const PORT = 8085;
